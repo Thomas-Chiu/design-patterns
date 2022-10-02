8d7fe62f37ebab73b0d9b7d0aa5d723b483d2d3e
Thomas-Chiu <josemich0511@gmail.com>
2022-10-03 00:08:46 +0800

    feat: add proxy example

--- structural/proxy/index.ts
+++ structural/proxy/index.ts
@@ -1,84 +1,40 @@
-/**
- * The Subject interface declares common operations for both RealSubject and the
- * Proxy. As long as the client works with RealSubject using this interface,
- * you'll be able to pass it a proxy instead of a real subject.
- */
-interface Subject {
-  request(): void;
+// Subject
+interface MO {
+  request(moNumber: string): void;
 }
 
-/**
- * The RealSubject contains some core business logic. Usually, RealSubjects are
- * capable of doing some useful work which may also be very slow or sensitive -
- * e.g. correcting input data. A Proxy can solve these issues without any
- * changes to the RealSubject's code.
- */
-class RealSubject implements Subject {
-  public request(): void {
-    console.log("RealSubject: Handling request.");
+// RealSubject
+class CreateMo implements MO {
+  public request(moNumber: string): void {
+    console.log(`creating MO ${moNumber}`);
   }
 }
 
-/**
- * The Proxy has an interface identical to the RealSubject.
- */
-class Proxy implements Subject {
-  private realSubject: RealSubject;
+// Proxy
+class MoProxy implements MO {
+  private mo: MO;
 
-  /**
-   * The Proxy maintains a reference to an object of the RealSubject class. It
-   * can be either lazy-loaded or passed to the Proxy by the client.
-   */
-  constructor(realSubject: RealSubject) {
-    this.realSubject = realSubject;
+  constructor() {
+    this.mo = new CreateMo();
   }
-
-  /**
-   * The most common applications of the Proxy pattern are lazy loading,
-   * caching, controlling the access, logging, etc. A Proxy can perform one of
-   * these things and then, depending on the result, pass the execution to the
-   * same method in a linked RealSubject object.
-   */
-  public request(): void {
-    if (this.checkAccess()) {
-      this.realSubject.request();
-      this.logAccess();
-    }
+  public request(moNumber: string): void {
+    this.preRequest();
+    this.mo.request(moNumber);
+    this.postRequest();
   }
-
-  private checkAccess(): boolean {
-    // Some real checks should go here.
-    console.log("Proxy: Checking access prior to firing a real request.");
-
-    return true;
+  public preRequest(): void {
+    console.log("check if MO is created");
   }
-
-  private logAccess(): void {
-    console.log("Proxy: Logging the time of request.");
+  public postRequest(): void {
+    console.log("MO created successfully");
   }
 }
 
+// Client
+const batteryMo = new MoProxy();
+batteryMo.request("B400500600");
 /**
- * The client code is supposed to work with all objects (both subjects and
- * proxies) via the Subject interface in order to support both real subjects and
- * proxies. In real life, however, clients mostly work with their real subjects
- * directly. In this case, to implement the pattern more easily, you can extend
- * your proxy from the real subject's class.
+  check if MO is created
+  creating MO B400500600
+  MO created successfully
  */
-function clientCode(subject: Subject) {
-  // ...
-
-  subject.request();
-
-  // ...
-}
-
-console.log("Client: Executing the client code with a real subject:");
-const realSubject = new RealSubject();
-clientCode(realSubject);
-
-console.log("");
-
-console.log("Client: Executing the same client code with a proxy:");
-const proxy = new Proxy(realSubject);
-clientCode(proxy);
